e5b246b3db107dd3e24a6a71f3b84bc06c87a2eb
josue28jrd <josue28jrd@hotmail.com>
2024-12-27 08:39:39 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,38 @@
 
-test = 500
+function generateKeyframes() {
+    const nColors = 150;
+    const colors = [];
 
-test > 300 ? console.log("maior 500") : console.log("menor 500")
+    for (let i = 0; i < nColors; i++) {
+        let r = 255 - Math.round(i * 255 / (nColors - 1));
+        let g = 0;
+        let b = Math.round(i * 255 / (nColors - 1));
+        colors.push(`rgba(${r}, ${g}, ${b}, 0.7)`);
+    }
 
+    for (let i = 0; i < nColors; i++) {
+        let r = Math.round(i * 255 / (nColors - 1));
+        let g = 0;
+        let b = 255 - Math.round(i * 255 / (nColors - 1));
+        colors.push(`rgba(${r}, ${g}, ${b}, 0.7)`);
+    }
+
+    let keyframes = "@keyframes pulseGradient {\n";
+    const totalSteps = colors.length;
+    const percentageStep = (100 / totalSteps).toFixed(2);
+
+    for (let i = 0; i < totalSteps; i++) {
+        const percentage = (i * percentageStep).toFixed(2) + "%";
+        keyframes += `    ${percentage} {\n        background: radial-gradient(circle, ${colors.join(", ")});\n    }\n`;
+    
+        const firstColor = colors.shift();
+        colors.push(firstColor);
+    }
+
+    keyframes += `    100% {\n        background: radial-gradient(circle, ${colors.join(", ")});\n    }\n}`;
+
+    const styleSheet = document.styleSheets[0];
+    styleSheet.insertRule(keyframes, styleSheet.cssRules.length);
+
+    document.querySelector('.background').style.animation = 'pulseGradient 5s infinite linear';
+} generateKeyframes();
